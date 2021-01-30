ce5fa77a3254b9fba5651dffaea338d465a7a38b
Arto Santala <crystoll@gmail.com>
2021-01-30 13:29:01 +0200

    More code vomit that we can refactor later. Added some state to get a real pomodoro feeling going.

--- mr_pomo_bot.py
+++ mr_pomo_bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import discord
 import os
+from timer import Timer
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -10,6 +11,7 @@ load_dotenv()
 #client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
+timer = Timer()
 
 
 #@client.event
@@ -22,7 +24,16 @@ async def on_ready():
 async def start_timer(ctx):
     start_work_em = discord.Embed(title="Time to start working!", color=0x33c633)
     await ctx.send(embed = start_work_em)
-    await asyncio.sleep(5)
+
+
+    timer.start()
+    while timer.get_status():
+        await asyncio.sleep(1) # 25 X 60
+        timer.tick()
+        if timer.get_ticks() >= 10:
+            timer.stop()
+
+
     start_play_em = discord.Embed(title="Time to start your break!", color=0x33c633)
     await ctx.send(embed = start_play_em)
 
@@ -31,6 +42,25 @@ async def start_timer(ctx):
 async def stop_timer(ctx):
     stop_timer_em = discord.Embed(title="Timer has been stopped!", color=0xc63333)
     await ctx.send(embed = stop_timer_em)
+    timer.stop()
+
+
+
+@bot.command(name="time", help="Show current time")
+async def show_time(ctx):
+    await ctx.send(f"Current timer status is : {timer.get_status()}")
+    await ctx.send(f"Current time is : {timer.get_ticks()}")
+
+
+
+@bot.command(name="help2", help="Show help text")
+async def show_help(ctx):
+    help_commands = dict()
+    for command in bot.commands:
+        help_commands[command.name] = command.help
+    description = "Bot commands are: {}".format(help_commands)
+    show_help_em = discord.Embed(title="This is Mr Pomo Dorio, a friendly Pomodoro bot", description=description, color=0xc63333)
+    await ctx.send(embed = show_help_em)
 
 
 
